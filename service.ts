2571d8f447dd8cae91e68e1fa6c22dd55aacc0c3
dryane <daniel.j.ryan.21@gmail.com>
2026-04-09 21:51:49 -0600

    lockscreen and bluetooth controller goes back to start of song on prev button

--- service.ts
+++ service.ts
@@ -1,10 +1,19 @@
 import TrackPlayer, { Event } from "react-native-track-player";
 
 module.exports = async function () {
+  console.log("[SERVICE] registered");
   TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
   TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
   TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
-  TrackPlayer.addEventListener(Event.RemotePrevious, () => TrackPlayer.skipToPrevious());
+  TrackPlayer.addEventListener(Event.RemotePrevious, async () => {
+    console.log("[SERVICE] RemotePrevious fired");
+    const progress = await TrackPlayer.getProgress();
+    if (progress.position > 5) {
+      await TrackPlayer.seekTo(0);
+    } else {
+      await TrackPlayer.skipToPrevious();
+    }
+  });
   TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
   TrackPlayer.addEventListener(Event.RemoteSeek, ({ position }) => {
     TrackPlayer.seekTo(position);
